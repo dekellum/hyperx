3048002422a96fd86d90eef1b58286189a6846f1
David Kellum <dek-oss@gravitext.com>
2019-03-26 15:33:42 -0700

    improve MSRV build.rs script

--- build.rs
+++ build.rs
@@ -1,15 +1,24 @@
 extern crate version_check;
 
+static PACKAGE: &str = "hyperx";
+static MSRV: &str = "1.27.2";
+
 fn main() {
-    match version_check::is_min_version("1.27.2") {
+    match version_check::is_min_version(MSRV) {
         Some((true, actual_v)) => {
-            eprintln!("hyperx MSRV test passed: {} (actual)", actual_v);
+            eprintln!(
+                "{} MSRV test passed: {} (actual)",
+                PACKAGE, actual_v);
         }
         Some((false, actual_v)) => {
-            panic!("hyperx MSRV is 1.27.2 > {} (this rustc)", actual_v);
+            panic!(
+                "{} MSRV is {} > {} (this rustc)",
+                PACKAGE, MSRV, actual_v);
         }
         None => {
-            panic!("couldn't query version info from rustc");
+            panic!(
+                "{}: couldn't query version info from rustc",
+                PACKAGE);
         }
     }
 }
