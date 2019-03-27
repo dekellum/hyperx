5fbba9524c96c92726ba4eb2311941184e0c2e01
David Kellum <dek-oss@gravitext.com>
2019-03-27 09:05:46 -0700

    improve MSRV build.rs script

--- build.rs
+++ build.rs
@@ -2,23 +2,26 @@ extern crate version_check;
 
 static PACKAGE: &str = "hyperx";
 static MSRV: &str = "1.27.2";
+static VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() {
+    static M_V: &str = "minimum supported rust version (MSRV)";
+
     match version_check::is_min_version(MSRV) {
         Some((true, actual_v)) => {
             eprintln!(
-                "{} MSRV test passed: {} (actual)",
-                PACKAGE, actual_v);
+                "{} v{} {} test passed: {} (actual rustc)",
+                PACKAGE, VERSION, M_V, actual_v);
         }
         Some((false, actual_v)) => {
             panic!(
-                "{} MSRV is {} > {} (this rustc)",
-                PACKAGE, MSRV, actual_v);
+                "{} v{} {} is {} > {} (this rustc)",
+                PACKAGE, VERSION, M_V, MSRV, actual_v);
         }
         None => {
             panic!(
-                "{}: couldn't query version info from rustc",
-                PACKAGE);
+                "{} v{}: couldn't query {} from rustc",
+                PACKAGE, VERSION, M_V);
         }
     }
 }
