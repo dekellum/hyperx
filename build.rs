760fb190c5bf622b2bda78eef8714132c2994432
David Kellum <dek-oss@gravitext.com>
2019-04-23 12:33:58 -0700

    replace version_check dep with minimal direct impl build.rs

--- build.rs
+++ build.rs
@@ -1,27 +1,52 @@
-extern crate version_check;
-
-static PACKAGE: &str = "hyperx";
-static MSRV: &str = "1.27.2";
-static VERSION: &str = env!("CARGO_PKG_VERSION");
+use std::env;
+use std::process::Command;
 
 fn main() {
-    static M_V: &str = "minimum supported rust version (MSRV)";
+    static PACKAGE: &'static str = "hyperx";
+    let msrv = vec![1, 27, 2];
 
-    match version_check::is_min_version(MSRV) {
-        Some((true, actual_v)) => {
-            eprintln!(
-                "{} v{} {} test passed: {} (actual rustc)",
-                PACKAGE, VERSION, M_V, actual_v);
-        }
-        Some((false, actual_v)) => {
-            panic!(
-                "{} v{} {} is {} > {} (this rustc)",
-                PACKAGE, VERSION, M_V, MSRV, actual_v);
-        }
-        None => {
-            panic!(
-                "{} v{}: couldn't query {} from rustc",
-                PACKAGE, VERSION, M_V);
+    static VERSION: &'static str = env!("CARGO_PKG_VERSION");
+    static M_V: &'static str = "minimum supported rust version (MSRV)";
+
+    let rustv = rustc_version();
+
+    if rustv < msrv {
+        panic!(
+            "{} v{} {} is {} > {} (this rustc)",
+            PACKAGE, VERSION, M_V, join(&msrv), join(&rustv));
+    }
+}
+
+fn join(ver: &Vec<u16>) -> String {
+    let mut out = String::new();
+    for v in ver {
+        if !out.is_empty() { out.push('.'); }
+        out.push_str(&v.to_string());
+    }
+    out
+}
+
+// Parse `rustc --version` and return as vector of integers, or panic.
+fn rustc_version() -> Vec<u16> {
+    let rustc = env::var("RUSTC").unwrap_or("rustc".to_owned());
+    let out = Command::new(rustc).arg("--version").output().unwrap();
+    let out = String::from_utf8(out.stdout).unwrap();
+    for l in out.lines() {
+        if l.starts_with("rustc ") {
+            let v = &l[6..];
+            if let Some(e) = v.find(" ") {
+                let v = &v[..e];
+                let mut vp = v.split("-");
+                if let Some(v) = vp.next() {
+                    let vs: Vec<u16> = v.split(".")
+                        .filter_map(|vss| vss.parse().ok())
+                        .collect();
+                    if !vs.is_empty() {
+                        return vs;
+                    }
+                }
+            }
         }
     }
+    panic!("rustc version not found")
 }
