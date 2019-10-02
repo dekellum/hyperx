3d6d4ede08629673b9ac13dc6421450d6c32b8e4
David Kellum <dek-oss@gravitext.com>
2019-10-02 12:53:24 -0700

    replace lock-min exception with unicase >= 2.1.0 upgrade

--- lock-minimize.sh
+++ lock-minimize.sh
@@ -2,18 +2,6 @@
 
 cargo -Z minimal-versions generate-lockfile
 
-# due to:
-# semver v0.1.0
-# └── rustc_version v0.1.0
-#     [build-dependencies]
-#     └── unicase v2.0.0
-#         ├── hyperx v0.15.2 (/home/david/src/hyperx)
-#         └── mime v0.3.2
-#             └── hyperx v0.15.2 (/home/david/src/hyperx) (*)
-# fixed directly, but never released
-# https://github.com/hyperium/mime/commit/c9d43b13c4a74b1168a268bd1e241d754992c15e
-cargo update -p unicase --precise 2.1.0
-
 # from iovec failure due to libc:
 # libc v0.2.1
 # ├── iovec v0.1.0
