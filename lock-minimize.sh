9b2a70ba1b3194f1545055711e7d5608bf44b6ba
David Kellum <dek-oss@gravitext.com>
2019-10-02 12:28:07 -0700

    annotate lock-minimize overrides

--- lock-minimize.sh
+++ lock-minimize.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/bash -ve
 
 cargo -Z minimal-versions generate-lockfile
+
+# due to:
+# semver v0.1.0
+# └── rustc_version v0.1.0
+#     [build-dependencies]
+#     └── unicase v2.0.0
+#         ├── hyperx v0.15.2 (/home/david/src/hyperx)
+#         └── mime v0.3.2
+#             └── hyperx v0.15.2 (/home/david/src/hyperx) (*)
 cargo update -p unicase --precise 2.1.0
+
+# from iovec failure due to libc:
+# libc v0.2.1
+# ├── iovec v0.1.0
+# │   └── bytes v0.4.4
+# │       ├── http v0.1.0
+# │       │   └── hyperx v0.15.2 (/home/david/src/hyperx)
+# │       └── hyperx v0.15.2 (/home/david/src/hyperx) (*)
+# └── time v0.1.37
+# └── hyperx v0.15.2 (/home/david/src/hyperx) (*)
 cargo update -p libc --precise 0.2.3
