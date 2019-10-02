ae8c888588d9b5f2033f792d255d8598770f7e59
David Kellum <dek-oss@gravitext.com>
2019-10-02 12:58:39 -0700

    another annotation of lock-minimize

--- lock-minimize.sh
+++ lock-minimize.sh
@@ -11,4 +11,6 @@ cargo -Z minimal-versions generate-lockfile
 # │       └── hyperx v0.15.2 (/home/david/src/hyperx) (*)
 # └── time v0.1.37
 # └── hyperx v0.15.2 (/home/david/src/hyperx) (*)
+# A fix appears to have never been released:
+# https://github.com/carllerche/iovec/commit/b54dfbe145d41385ac8826819b3603ed27cd7612
 cargo update -p libc --precise 0.2.3
